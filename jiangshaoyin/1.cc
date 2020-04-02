e3d12e8928f7940b688d784873cec3bc9c0d9f25
JiangShaoYin <tooo_cold@163.com>
2020-04-02 19:50:39 +0800

    1

---
+++ jiangshaoyin/1.cc
@@ -0,0 +1,21 @@
+#pragma once
+# ifndef __FUNC_H__
+# define __FUNC_H__
+
+#include <string>
+#include <sstream>
+#include <string>
+#include <sstream>
+#include <list>
+#include <map>
+#include <stack>
+#include <set>
+#include "unordered_set"
+#include "queue"
+#include <vector>
+#include <string>
+#include <map>
+#include <iostream>
+#include <numeric>
+#include <unordered_map>
+using namespace std;
