f306bb9443fecb0eadae2e330bad1447bb51ac26
JiangShaoYin <tooo_cold@163.com>
2020-04-02 20:30:46 +0800

    1

--- jiangshaoyin/1.cc
+++ jiangshaoyin/1.cc
@@ -99,3 +99,44 @@ int main() {
 }
 
 
+
+#include <string>
+#include <sstream>
+#include <string>
+#include <sstream>
+#include <list>
+#include <map>
+#include <stack>
+#include <set>
+#include "unordered_set"
+#include "queue"
+#include <vector>
+#include <string>
+#include <map>
+#include <iostream>
+#include <numeric>
+#include <unordered_map>
+using namespace std;
+using LL = long long;
+int main() {
+    LL n, capacity;
+    cin >> n >> capacity;
+    vector<int> costs;
+    for (int i = 0; i < n; ++i) {
+        int tem;
+        cin >> tem;
+        costs.push_back(tem);
+    }
+    int ret = 0, i = 0, start = 0, flag = false;
+    while (capacity >= 0) {
+        if (capacity - costs[i] >= 0) {
+            ret++;
+            capacity -= costs[i];
+            flag = false;
+        }
+        i = (i + 1) % n; 
+
+    }
+    cout << ret  << endl;
+    return 0;
+}
