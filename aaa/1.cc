ad76c6ec7407463ae7da2a5ed889eb69c275f1fd
JiangShaoYin <tooo_cold@163.com>
2021-03-28 19:42:46 +0800

    1

--- aaa/1.cc
+++ aaa/1.cc
@@ -20,7 +20,8 @@ using namespace std;
 int bestMethod(vector<int>& finance_product_capacity, vector<int>& benifit, int n, int M) {
     vector<int> max_benifit(M + 1, 0);
     for (int i = 0; i < n; i++) {
-        for (int cur_c = benifit[i])
-        max_benifit
+        for (int cur_c = benifit[i]; cur_c <= M; cur_c++) {
+            max_benifit[cur_c] = max(max_benifit[cur_c], max_benifit[cur_c - finance_product_capacity[i]] + benifit[i]);
+        }
     }
 }
